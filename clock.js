582afa587dc113e38236f1a7a7ab647e1b3a0fd7
Sarhad Salam <SarhadSalam@users.noreply.github.com>
2017-12-02 00:11:00 -0500

    Update clock.js

--- clock.js
+++ clock.js
@@ -8,7 +8,7 @@ setTheCountdown('.civ-clock',((new Date("2017/12/14 14:30:00"))- (new Date()))/1
 setTheCountdown('.calc1-clock',((new Date("2017/12/18 14:30:00"))- (new Date()))/1000);
 setTheCountdown('.calc2-clock',((new Date("2017/12/19 09:30:00"))- (new Date()))/1000);
 
-setTheCountdown('.sha-bday',((new Date("2017/12/19 00:00:00"))- (new Date()))/1000);
+setTheCountdown('.sha-bday',((new Date("2017/12/19 00:00:01"))- (new Date()))/1000);
 function setTheCountdown(objectName, time) {
     var clock = $(objectName).FlipClock(time,{
         countdown: true,
