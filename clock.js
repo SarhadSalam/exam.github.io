1a425a6d2ea04efec7cf99dffa343448396a39e6
Sarhad Maisoon Salam <sarhadmaisoon18@gmail.com>
2017-12-01 23:56:15 -0500

    fixed stuff

--- clock.js
+++ clock.js
@@ -1,11 +1,11 @@
 //Lin Alg Date
 //6:30 PM December 08 2017
 
-setTheCountdown('.lin-clock',((new Date("2017/12/08 06:30:00"))- (new Date()))/1000);
-setTheCountdown('.esp-clock',((new Date("2017/12/11 06:30:00"))- (new Date()))/1000);
-setTheCountdown('.che-clock',((new Date("2017/12/13 02:30:00"))- (new Date()))/1000);
-setTheCountdown('.civ-clock',((new Date("2017/12/14 02:30:00"))- (new Date()))/1000);
-setTheCountdown('.calc1-clock',((new Date("2017/12/18 02:30:00"))- (new Date()))/1000);
+setTheCountdown('.lin-clock',((new Date("2017/12/08 18:30:00"))- (new Date()))/1000);
+setTheCountdown('.esp-clock',((new Date("2017/12/11 18:30:00"))- (new Date()))/1000);
+setTheCountdown('.che-clock',((new Date("2017/12/13 14:30:00"))- (new Date()))/1000);
+setTheCountdown('.civ-clock',((new Date("2017/12/14 14:30:00"))- (new Date()))/1000);
+setTheCountdown('.calc1-clock',((new Date("2017/12/18 14:30:00"))- (new Date()))/1000);
 setTheCountdown('.calc2-clock',((new Date("2017/12/19 09:30:00"))- (new Date()))/1000);
 
 function setTheCountdown(objectName, time) {
---
+++ clockE.js
@@ -0,0 +1,16 @@
+//Lin Alg Date
+//6:30 PM December 08 2017
+
+setTheCountdown('.lin-clock',((new Date("2017/12/08 14:00:00"))- (new Date()))/1000);
+setTheCountdown('.esp-clock',((new Date("2017/12/11 14:00:00"))- (new Date()))/1000);
+setTheCountdown('.che-clock',((new Date("2017/12/13 18:30:00"))- (new Date()))/1000);
+setTheCountdown('.civ-clock',((new Date("2017/12/15 09:30:00"))- (new Date()))/1000);
+setTheCountdown('.calc1-clock',((new Date("2017/12/18 14:00:00"))- (new Date()))/1000);
+setTheCountdown('.calc2-clock',((new Date("2017/12/19 14:00:00"))- (new Date()))/1000);
+
+function setTheCountdown(objectName, time) {
+    var clock = $(objectName).FlipClock(time,{
+        countdown: true,
+        clockFace: 'DailyCounter'
+    });
+}
