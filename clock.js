2ba831d8741e1b0c6e17239a4a52983e35ddb3b2
Sarhad Maisoon Salam <sarhadmaisoon18@gmail.com>
2017-12-07 21:58:41 -0500

    ALex's bday

--- clock.js
+++ clock.js
@@ -6,11 +6,11 @@ setTheCountdown('.esp-clock',((new Date("2017/12/11 18:30:00"))- (new Date()))/1
 setTheCountdown('.che-clock',((new Date("2017/12/13 14:30:00"))- (new Date()))/1000);
 setTheCountdown('.civ-clock',((new Date("2017/12/14 14:30:00"))- (new Date()))/1000);
 setTheCountdown('.calc1-clock',((new Date("2017/12/18 14:30:00"))- (new Date()))/1000);
-setTheCountdown('.calc2-clock',((new Date("2017/12/19 09:30:00"))- (new Date()))/1000);
+setTheCountdown('.calc2-clock',((new Date("2017/12/19 09:30:00"))- (new Date()))/1000)
 
 setTheCountdown('.sha-bday',((new Date("2017/12/25 00:00:01"))- (new Date()))/1000);
 function setTheCountdown(objectName, time) {
-    var clock = $(objectName).FlipClock(time,{
+var clock = $(objectName).FlipClock(time,{
         countdown: true,
         clockFace: 'DailyCounter'
     });
--- clockE.js
+++ clockE.js
@@ -6,7 +6,9 @@ setTheCountdown('.esp-clock',((new Date("2017/12/11 14:00:00"))- (new Date()))/1
 setTheCountdown('.che-clock',((new Date("2017/12/13 18:30:00"))- (new Date()))/1000);
 setTheCountdown('.civ-clock',((new Date("2017/12/15 09:30:00"))- (new Date()))/1000);
 setTheCountdown('.calc1-clock',((new Date("2017/12/18 14:00:00"))- (new Date()))/1000);
-setTheCountdown('.calc2-clock',((new Date("2017/12/19 14:00:00"))- (new Date()))/1000);
+setTheCountdown('.calc2-clock',((new Date("2017/12/19 14:30:00"))- (new Date()))/1000);
+setTheCountdown('.alex-clock',((new Date("2018/01/05 00:00:01"))- (new Date()))/1000);
+
 
 function setTheCountdown(objectName, time) {
     var clock = $(objectName).FlipClock(time,{
