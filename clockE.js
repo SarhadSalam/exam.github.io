36d696642a07c8a5534485a54d5f6bf1e51057af
Sarhad Salam <SarhadSalam@users.noreply.github.com>
2018-04-15 21:00:58 -0400

    Semester 2

--- clockE.js
+++ clockE.js
@@ -1,13 +1,13 @@
 //Lin Alg Date
 //6:30 PM December 08 2017
 
-setTheCountdown('.lin-clock',((new Date("2017/12/08 14:00:00"))- (new Date()))/1000);
-setTheCountdown('.esp-clock',((new Date("2017/12/11 14:00:00"))- (new Date()))/1000);
-setTheCountdown('.che-clock',((new Date("2017/12/13 18:30:00"))- (new Date()))/1000);
-setTheCountdown('.civ-clock',((new Date("2017/12/15 09:30:00"))- (new Date()))/1000);
-setTheCountdown('.calc1-clock',((new Date("2017/12/18 14:00:00"))- (new Date()))/1000);
+setTheCountdown('.lin-clock',((new Date("2018/04/17 14:00:00"))- (new Date()))/1000);
+setTheCountdown('.esp-clock',((new Date("2018/04/19 14:00:00"))- (new Date()))/1000);
+setTheCountdown('.che-clock',((new Date("2018/04/24 14:00:00"))- (new Date()))/1000);
+setTheCountdown('.civ-clock',((new Date("2017/04/26 18:30:00"))- (new Date()))/1000);
+setTheCountdown('.calc1-clock',((new Date("2017/04/30 09:30:00"))- (new Date()))/1000);
 setTheCountdown('.calc2-clock',((new Date("2017/12/19 14:30:00"))- (new Date()))/1000);
-setTheCountdown('.alex-clock',((new Date("2018/01/05 00:00:01"))- (new Date()))/1000);
+setTheCountdown('.alex-clock',((new Date("2019/01/05 00:00:01"))- (new Date()))/1000);
 setTheCountdown('.sha-clock',((new Date("2018/08/20 00:00:01"))- (new Date()))/1000);
 
 
