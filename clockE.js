abdb5a35d6d8fd2ee3814b0dba4dc2b28040a803
Sarhad Salam <SarhadSalam@users.noreply.github.com>
2018-04-15 21:01:32 -0400

    Update clockE.js

--- clockE.js
+++ clockE.js
@@ -4,8 +4,8 @@
 setTheCountdown('.lin-clock',((new Date("2018/04/17 14:00:00"))- (new Date()))/1000);
 setTheCountdown('.esp-clock',((new Date("2018/04/19 14:00:00"))- (new Date()))/1000);
 setTheCountdown('.che-clock',((new Date("2018/04/24 14:00:00"))- (new Date()))/1000);
-setTheCountdown('.civ-clock',((new Date("2017/04/26 18:30:00"))- (new Date()))/1000);
-setTheCountdown('.calc1-clock',((new Date("2017/04/30 09:30:00"))- (new Date()))/1000);
+setTheCountdown('.civ-clock',((new Date("2018/04/26 18:30:00"))- (new Date()))/1000);
+setTheCountdown('.calc1-clock',((new Date("2018/04/30 09:30:00"))- (new Date()))/1000);
 setTheCountdown('.calc2-clock',((new Date("2017/12/19 14:30:00"))- (new Date()))/1000);
 setTheCountdown('.alex-clock',((new Date("2019/01/05 00:00:01"))- (new Date()))/1000);
 setTheCountdown('.sha-clock',((new Date("2018/08/20 00:00:01"))- (new Date()))/1000);
