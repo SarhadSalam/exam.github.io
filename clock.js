f250d6645b76564be23e56af64aabe9ea4e5f108
Sarhad Salam <SarhadSalam@users.noreply.github.com>
2018-04-15 21:11:30 -0400

    Update clock.js

--- clock.js
+++ clock.js
@@ -5,7 +5,7 @@ setTheCountdown('.ece110-clock',((new Date("2018/04/23 18:30:00"))- (new Date())
 setTheCountdown('.aps105-clock',((new Date("2018/04/24 18:30:00"))- (new Date()))/1000);
 setTheCountdown('.mie100-clock',((new Date("2018/04/26 14:00:00"))- (new Date()))/1000);
 setTheCountdown('.eco101-clock',((new Date("2018/04/27 14:00:00"))- (new Date()))/1000);
-setTheCountdown('.mat187-clock',((new Date("2018/04/28 14:00:00"))- (new Date()))/1000);
+setTheCountdown('.mat187-clock',((new Date("2018/04/20 18:30:00"))- (new Date()))/1000);
 
 setTheCountdown('.sha-bday',((new Date("2018/04/20 00:00:01"))- (new Date()))/1000);
 function setTheCountdown(objectName, time) {
